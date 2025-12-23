19014c095f8995200c4a0c12e121d9302e720e0a
egemen <egemenylmz01@gmail.com>
2025-12-23 22:56:23 +0300

    fix: single export for ErrorMessage component

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,14 +1,5 @@
 import styles from './ErrorMessage.module.css';
 
-export default function ErrorMessage({ message = 'An error occurred' }) {
+export default function ErrorMessage({ message = 'Whoops, something went wrong! Please try reloading this page!' }) {
   return <div className={styles.wrapper}>{message}</div>;
-}
-import css from './ErrorMessage.module.css';
-
-export default function ErrorMessage({ message = "Whoops, something went wrong! Please try reloading this page!" }) {
-  return (
-    <div className={css.container}>
-      <p className={css.text}>{message}</p>
-    </div>
-  );
 }
